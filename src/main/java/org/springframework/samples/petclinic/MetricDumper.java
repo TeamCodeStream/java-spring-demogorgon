edbc577ec6db9c898b27eafdcbd0d3ee0ab67b38
Calvin A. Allen <calvinallen@newrelic.com>
2024-10-09 10:35:25 -0400

    Remove lombok and mapstruct / simplify
    This builds and runs now.

--- src/main/java/org/springframework/samples/petclinic/MetricDumper.java
+++ src/main/java/org/springframework/samples/petclinic/MetricDumper.java
@@ -3,8 +3,6 @@ package org.springframework.samples.petclinic;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Component
 public class MetricDumper {
 	@Scheduled(fixedDelay = 60000)
--- src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
@@ -1,17 +1,16 @@
 package org.springframework.samples.petclinic.mapper;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.samples.petclinic.dto.OwnerDto;
 import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface OwnerMapper {
-
-	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-	void updateOwnerFromDto(OwnerDto dto, @MappingTarget Owner entity);
-
+@Component
+public class OwnerMapper {
+	public void updateOwnerFromDto(OwnerDto dto, Owner entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setAddress(dto.getAddress());
+		entity.setCity(dto.getCity());
+		entity.setTelephone(dto.getTelephone());
+	}
 }
