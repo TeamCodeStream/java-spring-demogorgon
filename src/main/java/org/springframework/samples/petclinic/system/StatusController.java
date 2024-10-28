641d042a135bc997cd3e7858fc74e1a9b7d55052
Calvin A. Allen <calvinallen@newrelic.com>
2024-10-28 11:37:00 -0400

    Need to add status check to get healthy upstream

---
+++ src/main/java/org/springframework/samples/petclinic/system/StatusController.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.system;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class StatusController {
+	@GetMapping("/check")
+	@ResponseStatus(HttpStatus.OK)
+	public void Check() {
+		// all good
+	}
+
+}
